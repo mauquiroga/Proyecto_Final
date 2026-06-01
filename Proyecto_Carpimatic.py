31f5ada0a430af6f64eecd503b7908a884d1168c
mauquiroga <mauquiroga@gmail.com>
2026-06-01 13:52:47 -0400

    quinta carga

--- Proyecto_Carpimatic.py
+++ Proyecto_Carpimatic.py
@@ -1,3 +1,5 @@
+nombre="mau"
+contraseña="123"
 while True:
     print("""Bienvenido al Sistema
             ***Carpimatic***
@@ -6,24 +8,40 @@ while True:
             2.Registrarse
             3.Salir""")
     opcion=int(input(" "))
-    while True:
-        if opcion==1:
+   
+    if opcion==1:
             nombreusuario=input("Ingrese su Nombre: ")
             nombreusuario.lower()
             contraseñausuario=input("Ingrese una Contraseña: ")
             contraseñausuario.lower()
             if nombre==nombreusuario and contraseña==contraseñausuario:
-                print("***Acceso Concedido***")                            
-            else:
+                print("***Acceso Concedido***")
+                while True:
+                    print("""Hola por favor seleccione una opcion
+                      1.Registrar Material
+                      2.Verificar Inventario
+                      3. salir""")
+                    indice=int(input("ingrese opcion: "))
+                    if indice==1:
+                        materialnuevo=input("Nombre de la Madera ")
+                        materialnuevo.lower().strip()
+                        cantidadnuevo=input("Ingrese cantidad en M2 ")
+                        cantidadnuevo.lower().strip()
+                        print("Material Registrado")
+                    elif indice==2:
+                        print(materialnuevo)
+                        print(cantidadnuevo)
+                    elif indice==3:
+                        break
+            else: 
+                
                 print("usuario o contraseña invalida por favor intente de nuevo: ")
-        elif opcion==2:
+    elif opcion==2:
             nombre=input("registre su nombre de usuario: ")
             nombre.lower()
             contraseña=input("Registre su contraseña: ")
             contraseña.lower()
             print("**** Registrado Exitosamente ****")
-                    
-        else:
-            print("saliendo")
             break
-    break
+    else:
+            break
